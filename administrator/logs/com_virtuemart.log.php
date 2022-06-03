369b0d20d9106e506c3b38177f8cb77d8b64127f
PetrinSergei <54847414+SergeiPetrin@users.noreply.github.com>
2022-06-03 19:47:49 +0300

    Add files via upload

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,8 @@
+#
+#<?php die("Forbidden."); ?>
+
+2018-02-22 21:57:38 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://joomla4shop.test60minut.ru/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2018-02-22 21:57:40 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://joomla4shop.test60minut.ru/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2018-02-22 21:58:02 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://joomla4shop.test60minut.ru/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2018-02-22 22:01:36 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://joomla4shop.test60minut.ru/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2018-02-22 22:02:25 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://joomla4shop.test60minut.ru/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
